5c5fe7d51ca255146149a825631f4f9d49f6955f
Nitzan Bar <nitzanbar@macam.ac.il>
2013-08-12 16:46:41 +0300

    blocks/massaction allow use all course formats

--- block_massaction.php
+++ block_massaction.php
@@ -35,8 +35,13 @@ class block_massaction extends block_base {
     /**
      * @see block_base::applicable_formats()
      */
-    function applicable_formats() {
-        return array('course-view-weeks' => true, 'course-view-topics' => true, 'course-view-topcoll' => true, 'course-view-moonstone' => true);
+    public function applicable_formats() {
+        return array(
+            'course-view'    => true,
+            'site'           => false,
+            'mod'            => false,
+            'my'             => false
+        );
     }
 
 
